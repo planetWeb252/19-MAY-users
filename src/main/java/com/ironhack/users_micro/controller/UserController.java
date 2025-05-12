595958882a496ecc79bab4a07ca4d2dbc3ccc28f
Marshall-Bits <kz_@hotmail.es>
2025-05-12 15:33:07 +0200

    initial commit

---
+++ src/main/java/com/ironhack/users_micro/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.ironhack.users_micro.controller;
+
+import com.ironhack.users_micro.model.User;
+import com.ironhack.users_micro.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/ironhack/users_micro/service/UserService.java
@@ -0,0 +1,24 @@
+package com.ironhack.users_micro.service;
+
+import com.ironhack.users_micro.model.User;
+import com.ironhack.users_micro.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
